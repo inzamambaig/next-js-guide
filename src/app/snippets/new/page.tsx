7b0f6218bec0b5586ca0f12237299221447a852b
InzamamBaig <baigsone@gmail.com>
2024-07-02 20:21:59 +0500

    Feat: Prima client Create snippet html form

---
+++ src/app/snippets/new/page.tsx
@@ -0,0 +1,32 @@
+export default function CreateSnippetPage() {
+  return (
+    <form className="">
+      <h3 className="font-bold mb-3 text-center">Create a snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="title">
+            Title
+          </label>
+          <input
+            type="text"
+            name="title"
+            className="border rounded p-2 w-full"
+            id="title"
+          />
+        </div>
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="code">
+            Code
+          </label>
+          <textarea
+            name="code"
+            className="border rounded p-2 w-full"
+            id="code"
+          />
+        </div>
+
+        <button className="rounded p-2 bg-blue-200 ">Add</button>
+      </div>
+    </form>
+  );
+}
