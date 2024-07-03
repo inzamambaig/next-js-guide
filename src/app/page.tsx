b8e88744c2e3bccb0441e910da1bd45a3ddae8fc
InzamamBaig <baigsone@gmail.com>
2024-07-04 00:58:48 +0500

    Refactor: Snippets list Added columns, new button and link to individual snnipet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,33 @@
+import Link from "next/link";
 import { db } from "@/db";
 
 export default async function Home() {
   const snippets = await db.snippet.findMany();
 
   const renderSnippets = snippets.map((snippet) => {
-    return <div key={snippet.id}>{snippet.title}</div>;
+    return (
+      <Link
+        href={`/snippets/${snippet.id}`}
+        key={snippet.id}
+        className="flex justify-between items-center p-2 border rounded mb-2"
+      >
+        <div>{snippet.title}</div>
+        <div>View </div>
+      </Link>
+    );
   });
-  return <div>{renderSnippets}</div>;
+  return (
+    <div>
+      <div className="flex justify-between items-center mb-2 ">
+        <h1 className="text-xl font-semibold">Snippets</h1>
+        <Link
+          href={`/snippets/new`}
+          className="border p-2 rounded hover:bg-gray-100"
+        >
+          New
+        </Link>
+      </div>
+      {renderSnippets}
+    </div>
+  );
 }
